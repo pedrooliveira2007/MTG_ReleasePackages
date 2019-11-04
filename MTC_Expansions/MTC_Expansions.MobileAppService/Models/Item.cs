cbfb71c34360869033fc4c4b1b7bea5707d3e52c
Pedro <Oliveira>
2019-11-04 09:22:33 +0000

    initial commit

---
+++ MTC_Expansions/MTC_Expansions.MobileAppService/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MTC_Expansions.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ MTC_Expansions/MTC_Expansions/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MTC_Expansions.Models;
+
+namespace MTC_Expansions.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
